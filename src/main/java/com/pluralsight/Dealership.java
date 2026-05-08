059895fa9e82293ebc12f4d44fbce12fe381b2df
Your Name <youremail@example.com>
2026-05-08 14:57:56 -0400

    I finished Phase 1. I completed the 'Dealership' class and 'Vehicle' class. I set up the attributes for both classes, created constructors for both classes, generated the getters and setters for both classes. For the 'Dealership' class I created the ArrayList for the vehicle and instantiated it in the constructor. Then underneath my getters and setters for the attributes I created methods to get the vehicle price, make/model, year, color, miles, type, and all vehicles. I made the methods return type 'List<Vehicle>' so it can return all the vehicles that match. I then created a 'addVehicle' method and a 'removeVehicle' method. For the 'addVehicle' method I added functionality as directed by calling inventory.add(vehicle) which will add a vehicle to the list. the rest of the methods return null.

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,79 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    //return type so I can take/reurn something from the list
+    public List<Vehicle> getVehicleByPrice (double min, double max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByYear (double min, double max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByColor (String color) {
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByMiles (double min, double max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByType (String type) {
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+    public void removeVehicle(Vehicle vehicle) {
+    }
+
 }
