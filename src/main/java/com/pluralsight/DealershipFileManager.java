5c4f8e6c13fc4c6eac13735f05ece246c7bec0c2
Your Name <youremail@example.com>
2026-05-08 17:15:44 -0400

    I created the 'getDealership' method and the 'saveDealership' method, for the 'saveDealership' I left it empty as instructed. Then I started to build the 'getDealership', I made a try with resource for the bufferReader, then read the first line of the file, split the text by the pipe '|' and creat the object 'dealership'. Made a while loop and called the helper method I built 'getVehicle' that splits the vin, year, make, etc. by the pipe '|' and created a vehicle, and then added the vehicle to the list.

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,55 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class DealershipFileManager {
+
+    public Dealership getDealership() {
+
+        //declared outside to be used anywhere
+        String fileName = "dealership.txt";
+        Dealership dealership = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            //reads first line(name,address,phone)
+            line = br.readLine();
+            String[] data = line.split("\\|");
+            String name = data[0];
+            String address = data[1];
+            String phone = data[2];
+            //creat object
+            dealership = new Dealership(name, address, phone);
+
+            while ((line = br.readLine()) != null) {
+                //loops though vehicle list
+                Vehicle vehicle = getVehicle(line);
+                //adds the vehicles
+                dealership.addVehicle(vehicle);
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return dealership;
+    }
+
+    private static Vehicle getVehicle(String line) {
+        String[] data = line.split("\\|");
+        int vin = Integer.parseInt(data[0]);
+        int year = Integer.parseInt(data[1]);
+        String make = data[2];
+        String model = data[3];
+        String vehicleType = data[4];
+        String color = data[5];
+        int odometer = Integer.parseInt(data[6]);
+        double price = Double.parseDouble(data[7]);
+
+        Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+        return vehicle;
+    }
+
+    public void saveDealership(Dealership dealership) {
+
+    }
 }
